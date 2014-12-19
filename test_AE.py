23967bd86b0a08a4e1b5f6539a59c8434f4dce8e
Marc-Andre Gardner <marc.andre.gardner@gmail.com>
2014-12-19 15:46:48 -0500

    Ajout de argparse, clean-up du fichier

--- test_AE.py
+++ test_AE.py
@@ -53,6 +53,8 @@ try:
 except ImportError:
     import Image
 
+import argparse
+
 
 
 class DatasetImporter:
@@ -376,8 +378,8 @@ class dA(object):
                 correctly as it only support float32 for now.
 
         """
-        return self.theano_rng.binomial(size=input.shape, n=1,
-                                        p=1 - corruption_level,
+
+        return self.theano_rng.normal(size=input.shape, avg=1., std=corruption_level,
                                         dtype=theano.config.floatX) * input
 
     def get_hidden_values(self, input):
@@ -391,23 +393,26 @@ class dA(object):
         """
         return T.nnet.sigmoid(T.dot(hidden, self.W_prime) + self.b_prime)
 
-    def get_cost_updates(self, corruption_level, learning_rate):
+    def get_cost_updates(self, corruption_level, learning_rate, costInit=None):
         """ This function computes the cost and the updates for one trainng
         step of the dA """
 
-        tilde_x = self.get_corrupted_input(self.x, corruption_level)
-        y = self.get_hidden_values(tilde_x)
-        z = self.get_reconstructed_input(y)
-        # note : we sum over the size of a datapoint; if we are using
-        #        minibatches, L will be a vector, with one entry per
-        #        example in minibatch
-        L = - T.sum(self.x * T.log(z) + (1 - self.x) * T.log(1 - z), axis=1)
-        # note : L is now a vector, where each element is the
-        #        cross-entropy cost of the reconstruction of the
-        #        corresponding example of the minibatch. We need to
-        #        compute the average of all these to get the cost of
-        #        the minibatch
-        cost = T.mean(L)
+        if costInit is None:
+            tilde_x = self.get_corrupted_input(self.x, corruption_level)
+            y = self.get_hidden_values(tilde_x)
+            z = self.get_reconstructed_input(y)
+            # note : we sum over the size of a datapoint; if we are using
+            #        minibatches, L will be a vector, with one entry per
+            #        example in minibatch
+            L = - T.sum(self.x * T.log(z) + (1 - self.x) * T.log(1 - z), axis=1)
+            # note : L is now a vector, where each element is the
+            #        cross-entropy cost of the reconstruction of the
+            #        corresponding example of the minibatch. We need to
+            #        compute the average of all these to get the cost of
+            #        the minibatch
+            cost = T.mean(L)
+        else:
+            cost = T.mean(costInit)
 
         # compute the gradients of the cost of the `dA` with respect
         # to its parameters
@@ -418,114 +423,24 @@ class dA(object):
             for param, gparam in zip(self.params, gparams)
         ]
 
-        return (cost, updates)
-
-
-def genDatasetFromEXR(folder, cropWidth=20):
-    os.chdir(folder)
-    data = []
-    for f in glob('*-img.exr'):
-        f_mask = f.rsplit("-", 1)[0] + "-mask.exr"
-        f_mask_hdl = OpenEXR.InputFile(f_mask)
-        FLOAT = Imath.PixelType(Imath.PixelType.FLOAT)
-        Y = numpy.array(array.array('f', f_mask_hdl.channel("Y", FLOAT)).tolist())
-         
-        f_hdl = OpenEXR.InputFile(f)
-         
-        # Read the three color channels as 32-bit floats
-        FLOAT = Imath.PixelType(Imath.PixelType.FLOAT)
-        (R,G,B) = [array.array('f', f_hdl.channel(Chan, FLOAT)).tolist() for Chan in ("R", "G", "B")]
-        R = numpy.array(R)
-        G = numpy.array(G)
-        B = numpy.array(B)
-
-        dgray = R * 0.25 + G * 0.25 +B * 0.5
-        dgray = dgray/dgray.max()
-        dgray[Y<0.5] = 0
-        dgray = dgray.reshape(256,256)
-        dgray = dgray[cropWidth:-cropWidth, cropWidth:-cropWidth]
-
-        dgray = numpy.ravel(dgray)
-        data.append(dgray)    
-        
-    dataN = numpy.vstack(data).astype(numpy.float32)
-
-    with open('data2.pkl','wb') as f:
-        pickle.dump((dataN, (256-cropWidth*2)**2, len(data)), f, -1)
-
-    dataConvert = theano.shared(numpy.asarray(dataN,
-                                                dtype=theano.config.floatX),
-                                                borrow=True)    # Ca fait quoi ce parametre la?
-    print(dataN.shape)
-    return dataConvert, (256-cropWidth*2)**2, len(data)
-
-
-def genDatasetFromPKL(path):
-    with open(path,'rb') as f:
-        val = pickle.load(f)
-
-    dataConvert = theano.shared(numpy.asarray(val[0],
-                                                dtype=theano.config.floatX),
-                                                borrow=True)    # Ca fait quoi ce parametre la?
-    return dataConvert, val[1], val[2]
-
-
-def genDataset(folder, n=-1):
-
-    import os, os.path
-    import scipy.io
-    fichiers = os.listdir(folder)
-
-    data = []
-    sA = scipy.io.loadmat('../data/solidAngles')
-
-    i = 0
-    D = None
-    for fname in fichiers:
-        if not 'envmap.exr.mat' in fname:
-            continue
-        if i == n:
-            break
-        i += 1
-
-        dataRaw = scipy.io.loadmat(os.path.join(folder, fname))
-        d = dataRaw['envmap']*numpy.dstack((sA['solidAngles'], sA['solidAngles'], sA['solidAngles']))
-
-
-        dgray = d[:,:,0] * 0.25 + d[:,:,1] * 0.25 + d[:,:,2] * 0.5
-
-        dgray[~numpy.isnan(dgray)] = dgray[~numpy.isnan(dgray)]/dgray[~numpy.isnan(dgray)].max()
-        dfilter = dgray[~numpy.isnan(dgray)]
-
-        #print(dfilter.shape, dgray.shape)
-
-        # Ponderation plutot arbitraire...
-
-        data.append(dfilter)
-
-        if D is None:
-            D = len(dfilter)
-        else:
-            assert D == len(dfilter), "Dimensions size mismatch : {} / {}".format(D, len(dfilter))
-
-    dataN = numpy.vstack(data).astype(numpy.float32)
-    dataConvert = theano.shared(numpy.asarray(dataN,
-                                                dtype=theano.config.floatX),
-                                                borrow=True)    # Ca fait quoi ce parametre la?
-    print(dataN.shape)
-    return dataConvert, D, len(data)
+        return (cost, updates, gparams)
 
 
 
 
-def train_AE_multiplelayers(hidden_layers = [1200, 300, 60], 
-                            epochs=[25, 25, 30], 
-                            dataset="../data/20130823/",
-                            learning_rate=0.1):
 
-    dI = DatasetImporter('MAT', "../data/20130823/", "envmap.exr.mat", nanbehavior='remove')
-    dI = DatasetImporter('EXR', "../data/", "", nanbehavior='zeros', cropWidth=20)
-    dI = DatasetImporter('PKL', "../data", "data2.pkl")
+def train_AE_multiplelayers(params):
+    hidden_layers = params.hiddensize
+    epoch_max = params.epochs
+    dtype, dpath, dname = params.datasettype, params.datasetpath, params.datasetname
+    learning_rate = params.lrate
+
+    dI = DatasetImporter(dtype, dpath, dname, nanbehavior=params.nan, cropWidth=params.crop, savepkl=None if params.dumpparsedimgto == "" else params.dumpparsedimgto)
+
+
+    #dI = DatasetImporter('MAT', "../data/20130823/", "envmap.exr.mat", nanbehavior='remove')
+    #dI = DatasetImporter('EXR', "../data/", "", nanbehavior='zeros', cropWidth=20)
+    #dI = DatasetImporter('PKL', "../data", "data2.pkl")
     dI.load()
 
     data, D, N = dI.dataTheano, dI.D, dI.N
@@ -541,9 +456,11 @@ def train_AE_multiplelayers(hidden_layers = [1200, 300, 60],
     input_vals = data
 
     liste_params = []
+    liste_ae = []
     i = 0
 
-    for hl,ep in zip(hidden_layers, epochs):
+    for hl in hidden_layers:
+        ep = epoch_max
         print 'Begin layer'
         da = dA(
             numpy_rng=rng,
@@ -552,8 +469,9 @@ def train_AE_multiplelayers(hidden_layers = [1200, 300, 60],
             n_visible=D,
             n_hidden=hl
             )
+        liste_ae.append(da)
 
-        cost, updates = da.get_cost_updates(
+        cost, updates, grads = da.get_cost_updates(
             corruption_level=0.3,
             learning_rate=learning_rate
             )
@@ -586,249 +504,63 @@ def train_AE_multiplelayers(hidden_layers = [1200, 300, 60],
         with open('params_op_'+str(i)+'.txt', 'w') as f:
             pickle.dump( [(da.W.get_value()), da.b.get_value(), da.b_prime.get_value()], f)
 
-        new_inputs = da.get_hidden_values(input_vals).eval()
-        print(new_inputs.shape)
+        new_inputs = da.get_hidden_values(input_vals)
+        new_inputs_eval = new_inputs.eval()
+        print(new_inputs_eval.shape)
 
         # La sortie de cette couche devient l'entree de la prochaine
-        input_vals = theano.shared(numpy.asarray(new_inputs,
-                                        dtype=theano.config.floatX),
-                                        borrow=True)    # Ca fait quoi ce parametre la?
-        #for d in input_vals:
-         #   new_input_vals.append()
+        #input_vals = theano.shared(numpy.asarray(new_inputs,
+        #                                dtype=theano.config.floatX),
+        #                                borrow=True)    # Ca fait quoi ce parametre la?
 
-        #input_vals = numpy.vstack(new_input_vals)
-        #import pdb
-        #pdb.set_trace()
-        #print(input_vals.shape)
         D = hl
         i += 1
 
+    # Fine tuning
+    for ep in range(10):    # Nombre d'epochs pour le fine tuning
 
+        for img in dI.dataGray:
+            imgKeep = img
+            input = img
+            for layer in liste_ae:
+                input = layer.get_hidden_values(input)
+            for layer in reversed(liste_ae):
+                input = layer.get_reconstructed_input(input)
+            output = input.eval()
 
+            diff = output - imgKeep
+            for layer in reversed(liste_ae):
+                cost, updates, grads = layer.get_cost_updates(
+                    corruption_level=0.0,
+                    learning_rate=learning_rate,
+                    costInit=theano.shared(numpy.asarray(diff, dtype=theano.config.floatX), borrow=True)
+                    )
+                imgT = theano.shared(numpy.asarray(img, dtype=theano.config.floatX), borrow=True)
+                train_da = theano.function(
+                    [imgT],
+                    cost,
+                    updates=updates,
+                    givens={
+                        x: img
+                    }
+                )
 
-
-
-
-
-def test_dA(learning_rate=0.1, training_epochs=15,
-            dataset='mnist.pkl.gz',
-            batch_size=20, output_folder='dA_plots'):
-
-    """
-    This demo is tested on MNIST
-
-    :type learning_rate: float
-    :param learning_rate: learning rate used for training the DeNosing
-                          AutoEncoder
-
-    :type training_epochs: int
-    :param training_epochs: number of epochs used for training
-
-    :type dataset: string
-    :param dataset: path to the picked dataset
-
-    """
-
-    # DEBUT MAG
-    train_AE_multiplelayers()
-    exit()
-
-    data, D, N = genDataset("../data/20130823")
-    n_train_batches = N // batch_size
-    index = T.lscalar() 
-    x = T.matrix('x')
-
-
-    # LES VARIABLES DU TRAIN SET DOIVENT ETRE SHARED (VOIR logistic_sgd)
-
-    rng = numpy.random.RandomState(123)
-    theano_rng = RandomStreams(rng.randint(2 ** 30))
-
-    da = dA(
-        numpy_rng=rng,
-        theano_rng=theano_rng,
-        input=x,
-        n_visible=D,
-        n_hidden=200
-    )
-
-    cost, updates = da.get_cost_updates(
-        corruption_level=0.3,
-        learning_rate=learning_rate
-    )
-
-    train_da = theano.function(
-        [index],
-        cost,
-        updates=updates,
-        givens={
-            x: data[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    start_time = time.clock()
-
-    # go through training epochs
-    for epoch in xrange(training_epochs):
-        # go through trainng set
-        c = []
-        for batch_index in xrange(n_train_batches):
-            c.append(train_da(batch_index))
-
-        print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
-
-    end_time = time.clock()
-
-    training_time = (end_time - start_time)
-
-    print >> sys.stderr, ('The no corruption code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((training_time) / 60.))
-
-    # Ne fonctionne pas, parce la taille n'est pas proche d'un nombre carre...
-    image = Image.fromarray(
-        tile_raster_images(X=da.W.get_value(borrow=True).T,
-                           img_shape=(28, 28), tile_shape=(10, 10),
-                           tile_spacing=(1, 1)))
-    image.save('filters_corruption_0.png')
-
-
-
-    # SUPPRIMER CE EXIT SI ON VEUT CONTINUER
-    exit()
-
-
-
-    datasets = load_data(dataset)
-    train_set_x, train_set_y = datasets[0]
-
-    # compute number of minibatches for training, validation and testing
-    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-
-    # allocate symbolic variables for the data
-    index = T.lscalar()    # index to a [mini]batch
-    x = T.matrix('x')  # the data is presented as rasterized images
-
-    if not os.path.isdir(output_folder):
-        os.makedirs(output_folder)
-    os.chdir(output_folder)
-    ####################################
-    # BUILDING THE MODEL NO CORRUPTION #
-    ####################################
-
-    rng = numpy.random.RandomState(123)
-    theano_rng = RandomStreams(rng.randint(2 ** 30))
-
-    da = dA(
-        numpy_rng=rng,
-        theano_rng=theano_rng,
-        input=x,
-        n_visible=28 * 28,
-        n_hidden=500
-    )
-
-    cost, updates = da.get_cost_updates(
-        corruption_level=0.,
-        learning_rate=learning_rate
-    )
-
-    train_da = theano.function(
-        [index],
-        cost,
-        updates=updates,
-        givens={
-            x: train_set_x[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    start_time = time.clock()
-
-    ############
-    # TRAINING #
-    ############
-
-    # go through training epochs
-    for epoch in xrange(training_epochs):
-        # go through trainng set
-        c = []
-        for batch_index in xrange(n_train_batches):
-            c.append(train_da(batch_index))
-
-        print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
-
-    end_time = time.clock()
-
-    training_time = (end_time - start_time)
-
-    print >> sys.stderr, ('The no corruption code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((training_time) / 60.))
-    image = Image.fromarray(
-        tile_raster_images(X=da.W.get_value(borrow=True).T,
-                           img_shape=(28, 28), tile_shape=(10, 10),
-                           tile_spacing=(1, 1)))
-    image.save('filters_corruption_0.png')
-
-    #####################################
-    # BUILDING THE MODEL CORRUPTION 30% #
-    #####################################
-
-    rng = numpy.random.RandomState(123)
-    theano_rng = RandomStreams(rng.randint(2 ** 30))
-
-    da = dA(
-        numpy_rng=rng,
-        theano_rng=theano_rng,
-        input=x,
-        n_visible=28 * 28,
-        n_hidden=500
-    )
-
-    cost, updates = da.get_cost_updates(
-        corruption_level=0.3,
-        learning_rate=learning_rate
-    )
-
-    train_da = theano.function(
-        [index],
-        cost,
-        updates=updates,
-        givens={
-            x: train_set_x[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    start_time = time.clock()
-
-    ############
-    # TRAINING #
-    ############
-
-    # go through training epochs
-    for epoch in xrange(training_epochs):
-        # go through trainng set
-        c = []
-        for batch_index in xrange(n_train_batches):
-            c.append(train_da(batch_index))
-
-        print 'Training epoch %d, cost ' % epoch, numpy.mean(c)
-
-    end_time = time.clock()
-
-    training_time = (end_time - start_time)
-
-    print >> sys.stderr, ('The 30% corruption code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % (training_time / 60.))
-
-    image = Image.fromarray(tile_raster_images(
-        X=da.W.get_value(borrow=True).T,
-        img_shape=(28, 28), tile_shape=(10, 10),
-        tile_spacing=(1, 1)))
-    image.save('filters_corruption_30.png')
-
-    os.chdir('../')
 
 
 if __name__ == '__main__':
-    test_dA()
+    parser = argparse.ArgumentParser(description='Parser Autoencoder')
+    parser.add_argument('hiddensize', type=int, nargs='+', help="Taille des couches cachees")
+    parser.add_argument("--datasetpath", type=str, help="Dossier contenant le dataset (path)")
+    parser.add_argument("--datasetname", type=str, default="", help="Nom du fichier (ou pattern)")
+    parser.add_argument("--datasettype", type=str, default="MAT", help="MAT | EXR | PKL")
+    parser.add_argument("--epochs", type=int, default=15, help="Nombre d'epoques pour chaque couche")
+    parser.add_argument("--bruit", type=float, default=0.2, help="Bruit ajoute (std.var. de la gaussienne)")
+    parser.add_argument("--lrate", type=float, default=0.1, help="Learning rate")
+    parser.add_argument("--nan", type=str, default="zeros", help="zeros | remove")
+    parser.add_argument("--crop", type=int, default=0, help="Nombre de pixels a retirer de chaque cote")
+    parser.add_argument("--dumpparsedimgto", type=str, default="", help="Fichier dans lequel faire un dump des images traitees")
+    args = parser.parse_args()
+
+    train_AE_multiplelayers(args)
+
+
